19a3e683faed1533dbb2190384894fd21de0576c
shofiazilf <szilfanz69@gmail.com>
2021-11-23 22:12:13 +0700

    Mengerjakan Pertemuan2

---
+++ application/views/view_latihan1.php
@@ -0,0 +1,19 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Penjumlahan</title>
+</head>
+
+<body>
+    <center>
+        Belajar Membuat Penjumlahan <br>
+        Nilai1 = <?= $nilai1; ?><br>
+        Nilai2 = <?= $nilai2; ?><br>
+        Hasil dari penjumlahan diatas yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+    </center>
+</body>
+
+</html>
